9aaecc9b71f77d5c6b92b2973c5b8ab9e1f40ebb
Daria Dziubaltowska <daria305@gmail.com>
2021-01-03 14:56:22 +0100

    MNG-15 Write first version of getPokemonById

--- src/api/pokemons.js
+++ src/api/pokemons.js
@@ -1,16 +1,32 @@
-import { format } from "prettier"
-import { App } from "../app/App"
+import fetch from "cross-fetch"
 
-const POKEMON_API_BASE_URL = process.env.POKEMON_API_BASE_URL || "https://pokeapi.co/api/v2/";
+const POKEMON_API_BASE_URL = process.env.POKEMON_API_BASE_URL || "https://pokeapi.co/api/v2";
 
 export async function getPokemonById(id) {
-    const res = await fetch(`${POKEMON_API_BASE_URL}pokemon/${id}`);
-    const pokeData = await res.json()
-    const {name, types, forms} = pokeData;
-    return name + types + forms
+    const getTypeIdFromUrl = (url) => {
+        const regex = /\/type\/(\d+)\/$/;
+        return Number(regex.exec(url)[1])
+    }
+
+    const parseType = (type) => {
+        return {
+            id : getTypeIdFromUrl(type.type.url),
+            type : type.type.name
+        }
+    };
+
+    const res = await fetch(`${POKEMON_API_BASE_URL}/pokemon/${id}`);
+    const jsonRes = await res.json()
+    const { id: pokeId, name, types, sprites: { other: { "official-artwork": { front_default: artworkUrl} } } } = jsonRes; 
+    const pokeData = {
+        id: pokeId,
+        name: name,
+        types: types.map(parseType),
+        photoUrl: artworkUrl
+    }
+    return pokeData
 };
 
 export function getTypeById(id) {
     return id + "test"
-};
-
+}
