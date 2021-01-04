95d8a15cb6c3eddf6d7920f33e5650703bc2fad3
unknown <mariusz.smarz@gmail.com>
2021-01-04 22:21:27 +0100

    Add object scheme to QuestionService

--- src/service/QuestionService.js
+++ src/service/QuestionService.js
@@ -2,7 +2,7 @@ import {
     getPokemonById,
     //getTypeById
 } from "../api/pokemons";
-import { WHO_IS_THAT_POKEMON } from "./modes";
+import { WHAT_DOES_THIS_POKEMON_LOOK_LIKE, WHO_IS_THAT_POKEMON } from "./modes";
 
 
 // example result of calling getPokemonById(1):
@@ -29,51 +29,23 @@ export class QuestionService {
         if (!pokemonIds || pokemonIds.length != 4) {
             throw new Error('pokemonIds is not an array of 4 elements')
         }
-        const answersList = this.answers();
-        const correctAnswerId = answersList[this.correctAnswer() - 1];
-        console.log("Correct answer ID value " + correctAnswerId);
-
-        // Created to see what will be print
-        // getPokemonById(correctAnswerId).then(meta => {
-        //     const correctAnswerPoke = meta;
-        //     console.log(correctAnswerPoke);
-        // });
-
-        console.log("answersList: " + answersList)
-        console.log("Is Array: " + Array.isArray(answersList))
-
-        const pokePromises = answersList.map( id => getPokemonById(id))
-        console.log("Promises: " + pokePromises)
-
+        
+        const pokePromises = pokemonIds.map( id => getPokemonById(id))
         const answersObj = await Promise.all(pokePromises);
-
-        console.log("Answer 1: " + answersObj[0].name);
         
         if (this.mode === WHO_IS_THAT_POKEMON) {
-            var currQuestion = {
-                pokemonUrl: getPokemonById(correctAnswerId).photoUrl
-            };
-
-            var answers = answersList.map( (id) => getPokemonById(id).name);
-            var correctAnswer = getPokemonById(correctAnswerId).name;
-
-        } else if (this.mode === 2) {
-            var currQuestion = {
-                pokemonName: getPokemonById(correctAnswerId).name
-            };
-
-            var answers = answersList.map( (id) => getPokemonById(id).photoUrl);
-            var correctAnswer = getPokemonById(correctAnswerId).photoUrl;
+            return {
+                question: answersObj[0].photoUrl , 
+                answers: [ answersObj[0].name, answersObj[1].name, answersObj[2].name, answersObj[3].name ], 
+                correctAnswer: { value: answersObj[0].name, index: 0}
+            }
+        } else if (this.mode === WHAT_DOES_THIS_POKEMON_LOOK_LIKE ) {
+            return {
+                question: answersObj[0].name , 
+                answers: [ answersObj[0].photoUrl, answersObj[1].photoUrl, answersObj[2].photoUrl, answersObj[3].photoUrl ], 
+                correctAnswer: { name: answersObj[correctAnswer.index].photoUrl, index: 0 }
+            }
         };
-        
-        const question = {
-            question: currQuestion,
-            answers: answers, 
-            correctAnswer: correctAnswer 
-        };
-
-        return question;
-
     }
 
     checkAnswer(question, userAnswer) {
--- src/service/QuestionService.spec.js
+++ src/service/QuestionService.spec.js
@@ -7,7 +7,7 @@ const quizQuestion = new QuestionService(WHO_IS_THAT_POKEMON);
 
 describe('Test getNextQuestion method', () => {
 
-    it("Should return question object - example output for mode 1: question: 'Bulbasaur' , answers: [ 'answer1', 'answer2', 'answer3', 'answer4', 'answer5-forTypes', 'answer6-forTypes', ], correctAnswer: 'Bulbasaur'  ", async () => {
+    it("Should return question object - example output for mode 1: question: 'bulbasaur' , answers: [ 'answer1', 'answer2', 'answer3', 'answer4' ], correctAnswer: { name: 'bulbasaur', index: 1 }  ", async () => {
         // Given
         const pokomonIds = [1, 2, 3, 4];
 
@@ -15,18 +15,18 @@ describe('Test getNextQuestion method', () => {
         const question = await quizQuestion.getNextQuestion(pokomonIds);
 
         // Then
-        expect(question).toHaveAttribute('question');
-        expect(question.question).toEqual('bulbasaur');
+        expect(question).toHaveProperty('question');
+        expect(question.question).toEqual('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png');
 
-        expect(question).toHaveAttribute('answers');
+        expect(question).toHaveProperty('answers');
         expect(question.answers).toHaveLength(4);
         expect(question.answers).toEqual(expect.arrayContaining(['bulbasaur', 'ivysaur', 'venusaur', 'charmander'])); //In any order
         
-        expect(question).toHaveAttribute('correctAnswer');
-        expect(question.correctAnswer).toHaveAttribute('name');
-        expect(question.correctAnswer.name).toEqual('bulbasaur');
-        expect(question.correctAnswer).toHaveAttribute('index');
+        expect(question).toHaveProperty('correctAnswer');
+        expect(question.correctAnswer).toHaveProperty('value');
+        expect(question.correctAnswer.value).toEqual('bulbasaur');
+        expect(question.correctAnswer).toHaveProperty('index');
         
-        expect(question.answers[question.correctAnswer.index]).toEqual(question.correctAnswer.name);
+        expect(question.answers[question.correctAnswer.index]).toEqual(question.correctAnswer.value);
     })
 });
