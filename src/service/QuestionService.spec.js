a4b73902627e8d126a7d5f98ffbb3421bc4fee70
unknown <mariusz.smarz@gmail.com>
2021-01-04 22:57:55 +0100

    Modify import in test file

--- src/service/QuestionService.spec.js
+++ src/service/QuestionService.spec.js
@@ -1,7 +1,7 @@
 import { WHO_IS_THAT_POKEMON } from "./modes";
 import {
     QuestionService
-} from "./questionService";
+} from "./questionService.js";
 
 const quizQuestion = new QuestionService(WHO_IS_THAT_POKEMON);
 
