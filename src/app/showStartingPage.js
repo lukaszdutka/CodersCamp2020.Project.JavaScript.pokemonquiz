1db1057ac5678bd0596c5fa1b7aaa3772d400cd4
AgataLudwiczynska <74932979+AgataLudwiczynska@users.noreply.github.com>
2021-01-04 19:43:06 +0100

    Responsiveness + little corrections

--- src/app/showStartingPage.js
+++ src/app/showStartingPage.js
@@ -1,31 +1,34 @@
 export const showStartingPage = () => {
     const appScreen = document.querySelector('#pokequiz-app');
     const startingPageTemplate = 
-    `<div id="headerWithLogo">
+    `<div id="headerWithLogo" class="firstColumn spanInPortrait">
         <img src="https://flarrow.pl/wp-content/uploads/2019/07/International_Pok%C3%A9mon_logo.svg_.png" alt='Pokemon' id='pokemonLogo'/>
         <p id='quizLogo' class='fancyFontStyle'>Quiz</p>
     </div>
 
-        <ul id="selectModeMenu">
+        <ul id="selectModeMenu" class="firstColumn spanInPortrait">
             <li id='whoIsThatPokemonOption' class="buttonWithText">Who's that pokemon?</li>
             <li id='whatItLooksLikeOption' class="buttonWithText">What it looks like?</li>
             <li id='guessTheTypeOption' class="buttonWithText">Guess the type!</li>
         </ul>
 
-    <div id='enterNameAndPlayMenu'>
+    <div id='enterNameAndPlayMenu' class="secondColumn spanInPortrait">
         <p id='enterYourName' class='fancyFontStyle'>ENTER YOUR NAME</p>
         <div id='enterYourNameArea'>
             <img src='https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png' alt='playerIcon' id='playerIcon'/>
             <input type='text' id='enterYourNameInput'>
         </div>
+    </div>
+
+    <div id='startGame' class="secondColumn spanInPortrait">
         <input type='button' id='startGameButton' class="buttonWithText" value='PLAY'>
     </div>
 
-    <ul class='bottomOfThePageOptions'>
+    <ul class='bottomOfThePageOptions' class="firstColumn">
         <li id='helpOption'>?</li>
         <li id='leaderboard'><i class="fa fa-trophy"></i></li>
     </ul>
 
-    <img src='https://assets.pokemon.com//assets/cms2/img/video-games/_tiles/pokemon-sword-shield/distributions/pikachu/inline/world.png' alt='Pikachu' id='pikachuImg'/>`
+    <img src='https://assets.pokemon.com//assets/cms2/img/video-games/_tiles/pokemon-sword-shield/distributions/pikachu/inline/world.png' alt='Pikachu' id='pikachuImg' class="secondColumn"/>`
     appScreen.innerHTML = startingPageTemplate;
 }
