722ab0eaf8f0a4b0176aad6125f95f012a7a3eb3
mariusz-sm <mariusz.smarz@gmail.com>
2021-01-05 19:58:56 +0100

    Modify modes

--- src/service/modes.js
+++ src/service/modes.js
@@ -1,2 +1,14 @@
-export const WHO_IS_THAT_POKEMON = 'WHO_IS_THAT_POKEMON';
-export const WHAT_DOES_THIS_POKEMON_LOOK_LIKE = 'WHAT_DOES_THIS_POKEMON_LOOK_LIKE';
+export const WHO_IS_THAT_POKEMON = {
+    name: 'WHO_IS_THAT_POKEMON',
+    title: "Who's that pokemon?",
+    questionType: "image",
+    answerType: "text",
+    answersNumber: 4
+};
+export const WHAT_DOES_THIS_POKEMON_LOOK_LIKE = {
+    name: WHAT_DOES_THIS_POKEMON_LOOK_LIKE,
+    title: "What does this pokemon look like?",
+    questionType: "image",
+    answerType: "text",
+    answersNumber: 4
+};
