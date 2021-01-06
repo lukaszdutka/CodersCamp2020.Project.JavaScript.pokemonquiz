f1a1788c09c39417dbd55a542beef843bbcb96e5
Daria Dziubaltowska <daria305@gmail.com>
2021-01-06 09:45:05 +0100

    MNG-11 All finished before user selects the answer

--- src/app/quizPage.js
+++ src/app/quizPage.js
@@ -13,27 +13,41 @@ export function renderQuizPage(mode) {
             index: 1
         }
     }
+
+    const quizQuestion2 = {
+        question: 'bulbasaur' ,
+        answers: ['https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+         'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'],
+        correctAnswer: {
+            name: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+            index: 1
+        }
+    }
+
     const generatedQuestion = {
         question: quizQuestion,
         questionNum: 1,
     }
-    setupPageTitle(mode)
-    //TODO setupTimer()
-    renderNextQuestion(generatedQuestion, mode)
+
+    setupPageTitle(mode);
+    //TODO setupTimer() -- here or directly in App
+    renderNextQuestion(generatedQuestion, mode);
 }
 
 // use to update quizPage and change only the question, new answers and question qounter
 // not changing the time bar
 export function renderNextQuestion(genQuestion, mode) {
     const quizBody = document.querySelector("#quiz-body");
-    const quizQuestionElem = quizBody.querySelector(".quiz-question")
-    updateQuestion(quizQuestionElem, genQuestion.question, mode)
+    const quizQuestionElem = quizBody.querySelector(".quiz-question");;
+    updateQuestion(quizQuestionElem, genQuestion.question, mode);
 
-    const quizUl = quizBody.querySelector(".quiz-answers-list")
-    updateAnswersList(quizUl, genQuestion.question, mode)
+    const quizUl = quizBody.querySelector(".quiz-answers-list");
+    updateAnswersList(quizUl, genQuestion.question, mode);
 
-    // const questionCounter = quizBody.querySelector("#question-counter");
-    // updateQuestionCounter(questionCounter, questionNum)
+    const questionCounter = document.querySelector("#question-counter");
+    updateQuestionCounter(questionCounter, genQuestion.questionNum);
 }
 
 // Changes the title corresponding to the chosen game mode
@@ -45,31 +59,32 @@ const setupPageTitle = (mode) => {
 // Updates the question div with styling and content depending on a type of a question
 const updateQuestion = (questionElement, questionSet, mode) => {
     if (mode.questionType === "image") {
-        questionElement.classList.add("question-img")
-        const imgElem = createImgElement(questionSet.question)  // add img from url
-        questionElement.appendChild(imgElem)
+        questionElement.classList.add("question-img");
+        const imgElem = createImgElement(questionSet.question);  // add img from url
+        questionElement.appendChild(imgElem);
 
     } else if (mode.questionType === "text") {
-        questionElement.classList.add("question-text")
-        questionElement.innerText = questionSet.question.question  // add question as an inner text
+        questionElement.classList.add("question-text");
+        questionElement.innerText = questionSet.question.question;  // add question as an inner text
     }
 }
 
 // Creates img element with a selected image url
 const createImgElement = (url) => {
-    const img = document.createElement("img")
-    img.setAttribute("src", url)
-    return img
+    const img = document.createElement("img");
+    img.setAttribute("src", url);
+    return img;
 }
 
 // Updates styles for question list based on mode type
 // creates question items sor provided question set
 const updateAnswersList = (answersElement, questionSet, mode) => {
-    // current version works only for text answer type
-    //TODO add img version
-    
+   
     if (mode.questionType == "text") {
-        answersElement.classList.add("quiz-answer-text")
+        answersElement.classList.add("quiz-answer-text");
+
+    } else if (mode.questionType == "image") {
+        answersElement.classList.add("quiz-answer-img");
     }
     for (let answer of questionSet.answers) {
         const answerElement = createAnswerElement(answer, mode);
@@ -77,7 +92,7 @@ const updateAnswersList = (answersElement, questionSet, mode) => {
     }
 }
 
-// returns first child element from tthe template
+// returns first child element from the template
 // for template.content replacement, which is not fully supported yet
 const getTemplateContent = (template) => {
     const dummyDiv = document.createElement("div");  // 
@@ -103,4 +118,9 @@ const createAnswerElement = (answer, mode) => {
         liFirstElem.innerText = answer  // add question as an inner text
     }
     return li
+}
+
+// Updates the question number ona quiz page
+const updateQuestionCounter = (counterElem, questionNum) => {
+    counterElem.innerText = String(questionNum).padStart(2, '0');  //TODO maybe - total num of question in a game mode
 }
