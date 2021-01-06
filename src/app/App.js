98dbfe0bc6316273a339d4be705515fe7ef7c7b1
AgataLudwiczynska <74932979+AgataLudwiczynska@users.noreply.github.com>
2021-01-06 22:17:54 +0100

    Correction

--- src/app/App.js
+++ src/app/App.js
@@ -16,20 +16,20 @@ export const App = ({options}) => {
    const help=document.querySelector('#helpScreen')
    const style = getComputedStyle(help);
    document.querySelector('#whoIsThatPokemonOption').addEventListener('click',()=>{
-      if(style.display=='none'||document.querySelector('#helpScreen').style.display == 'none')
+      if(style.display=='none'||help.style.display == 'none')
          console.log("Who's that Pokemon?");
    });
    document.querySelector('#whatItLooksLikeOption').addEventListener('click',()=>{
-      if(style.display=='none'||document.querySelector('#helpScreen').style.display == 'none')
+      if(style.display=='none'||help.style.display == 'none')
          console.log("What it looks like?");
    });
    document.querySelector('#guessTheTypeOption').addEventListener('click',()=>{
-      if(style.display=='none'||document.querySelector('#helpScreen').style.display == 'none')
+      if(style.display=='none'||help.style.display == 'none')
          console.log("Guess the type!");
    });
    //ad event listener to the leaderboard button
    document.querySelector('#leaderboard').addEventListener('click',()=>{
-      if(style.display=='none'||document.querySelector('#helpScreen').style.display == 'none')
+      if(style.display=='none'||help.style.display == 'none')
          console.log("Hall of Fame");
    });
 }
