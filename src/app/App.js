336b9a13b1822e83b973508b8a3a3adf2a883d11
AgataLudwiczynska <74932979+AgataLudwiczynska@users.noreply.github.com>
2021-01-06 20:57:07 +0100

    print the name of clicked button

--- src/app/App.js
+++ src/app/App.js
@@ -12,6 +12,18 @@ export const App = ({options}) => {
    document.querySelector('#helpOption').addEventListener('click', () => {
       showAPopUpScreen(document.querySelector('#helpScreen'))
    });
+   document.querySelector('#whoIsThatPokemonOption').addEventListener('click',()=>{
+      console.log("Who's that Pokemon?");
+   });
+   document.querySelector('#whatItLooksLikeOption').addEventListener('click',()=>{
+      console.log("What it looks like?");
+   });
+   document.querySelector('#guessTheTypeOption').addEventListener('click',()=>{
+      console.log("Guess the type!");
+   });
+   document.querySelector('#leaderboard').addEventListener('click',()=>{
+      console.log("Hall of Fame");
+   });
 }
 
 
