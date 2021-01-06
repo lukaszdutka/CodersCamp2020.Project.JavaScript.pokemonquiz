99c74866a5b865c42c504bda3da83149fa595467
AgataLudwiczynska <74932979+AgataLudwiczynska@users.noreply.github.com>
2021-01-06 21:40:04 +0100

    no possibility to click on the button while a hel screen is active

--- src/app/App.js
+++ src/app/App.js
@@ -12,17 +12,25 @@ export const App = ({options}) => {
    document.querySelector('#helpOption').addEventListener('click', () => {
       showAPopUpScreen(document.querySelector('#helpScreen'))
    });
+   //add event listener to the select mode menu button 
+   const help=document.querySelector('#helpScreen')
+   const style = getComputedStyle(help);
    document.querySelector('#whoIsThatPokemonOption').addEventListener('click',()=>{
-      console.log("Who's that Pokemon?");
+      if(style.display=='none'||document.querySelector('#helpScreen').style.display == 'none')
+         console.log("Who's that Pokemon?");
    });
    document.querySelector('#whatItLooksLikeOption').addEventListener('click',()=>{
-      console.log("What it looks like?");
+      if(style.display=='none'||document.querySelector('#helpScreen').style.display == 'none')
+         console.log("What it looks like?");
    });
    document.querySelector('#guessTheTypeOption').addEventListener('click',()=>{
-      console.log("Guess the type!");
+      if(style.display=='none'||document.querySelector('#helpScreen').style.display == 'none')
+         console.log("Guess the type!");
    });
+   //ad event listener to the leaderboard button
    document.querySelector('#leaderboard').addEventListener('click',()=>{
-      console.log("Hall of Fame");
+      if(style.display=='none'||document.querySelector('#helpScreen').style.display == 'none')
+         console.log("Hall of Fame");
    });
 }
 
