d91df02ac40a7d777aef788918a5cf8c842e6bf6
Łukasz Dutka <lukaszdutka2@gmail.com>
2021-01-07 15:42:03 +0100

    Update src/app/addHelpScreenTemplate.js

--- src/app/addHelpScreenTemplate.js
+++ src/app/addHelpScreenTemplate.js
@@ -7,7 +7,7 @@ export const addHelpScreenTemplate = () => {
         <p class='exitPopUpScreen'>&#10005;</p>
         </div>
         <ul class='mainHelpTextSection'>
-            <li><span class='helpTitle'>Who's that pokemon?</span> During the quiz, you will have to quess the name of the pokemon displayed in the picture. The game ends after 2 minutes or after you answer 30 questions.</li>
+            <li><span class='helpTitle'>Who's that pokemon?</span> During the quiz, you will have to guess the name of the pokemon displayed in the picture. The game ends after 2 minutes or after you answer 30 questions.</li>
             <li><span class='helpTitle'>What it looks like?</span> During the quiz, you will have to select the correct picture of the pokemon whose name is displayed on the screen. The game ends after 2 minutes or after you answer 30 questions.</li>
             <li><span class='helpTitle'>Guess the type!</span> During the quiz, you will have to select all the pokemon types that the pokemon displayed on the screen belongs to (multiple choice is possible). The game ends after 2 minutes or after you answer 30 questions.</li>
         </ul>
