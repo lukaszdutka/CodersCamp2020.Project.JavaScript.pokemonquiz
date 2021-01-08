434181b2f51509e2e8a118803e7a58783a16c4a9
Daria Dziubaltowska <daria305@gmail.com>
2021-01-08 21:22:01 +0100

    MNG-44 Add GameHandler to store user game results

---
+++ src/service/GameHandler.js
@@ -0,0 +1,35 @@
+
+export class GameHandler {
+    constructor(name, totalTime) {
+        this.name = name;
+        this.totalTime = totalTime;
+        this.score = 0;
+        this.answers = [];
+    }
+
+    addAnswers(correctAnswer, answer, isCorrect) {
+        this.answers.push({
+            answer: answer,
+            correctAnswer: correctAnswer,
+            isCorrect: isCorrect,
+        })
+        if (this.isCorrect) {
+            this.score++;
+        }
+    }
+
+    // Subtruct time left from total time
+    // returns time of the game
+    calculateTime(timeLeft) {
+        return this.totalTime - timeLeft
+    }
+
+    getResults(timeLeft) {
+        return {
+            name: this.name,
+            time: this.calculateTime(timeLeft),
+            score: this.score,
+            answers: this.answers,
+        }
+    }
+}
