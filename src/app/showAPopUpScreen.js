0cf5cf776dc4d84f8e36d0f05a11cb243bc02fe7
AleksandraCyp <73715885+AleksandraCyp@users.noreply.github.com>
2021-01-10 18:45:42 +0100

    Design and style results modal

--- src/app/showAPopUpScreen.js
+++ src/app/showAPopUpScreen.js
@@ -4,14 +4,16 @@ export const showAPopUpScreen = (screenToDisplay) => {
     // add blur to the rest of the page
     document.querySelectorAll('.disableWithPopUpScreen').forEach(e => e.style.filter = 'blur(4px)');
    // function which gets called when the exit button is clicked
-    const exitPopUpFunction = () => {
-              //hide the screen
-              screenToDisplay.style.display = 'none';
-              // remove the blur
-              document.querySelectorAll('.disableWithPopUpScreen').forEach(e => e.style.filter = 'blur(0)');
-              // remove the event listener
-              screenToDisplay.querySelector('.exitPopUpScreen').removeEventListener('click', exitPopUpFunction);
+   if (screenToDisplay.querySelector('.exitPopUpScreen')) {
+        const exitPopUpFunction = () => {
+            //hide the screen
+            screenToDisplay.style.display = 'none';
+            // remove the blur
+            document.querySelectorAll('.disableWithPopUpScreen').forEach(e => e.style.filter = 'blur(0)');
+            // remove the event listener
+            screenToDisplay.querySelector('.exitPopUpScreen').removeEventListener('click', exitPopUpFunction);
     };
-        // add event listener to the exit button;
+    // add event listener to the exit button;
     screenToDisplay.querySelector('.exitPopUpScreen').addEventListener('click', exitPopUpFunction);
+    }
  };
