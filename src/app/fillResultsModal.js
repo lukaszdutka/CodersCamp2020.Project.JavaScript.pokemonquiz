ae828eaf519cbf24c399a88aa52da1978994cda8
AleksandraCyp <73715885+AleksandraCyp@users.noreply.github.com>
2021-01-13 21:47:36 +0100

    Works with both modes

--- src/app/fillResultsModal.js
+++ src/app/fillResultsModal.js
@@ -3,23 +3,35 @@ export const fillResultsModal = (gameHandlerResults) => {
     const resultsDescription = document.querySelector('#resultsDescription');
     resultsDescription.textContent = `Congratulations ${gameHandlerResults.name}! During ${gameHandlerResults.time} seconds you answered ${gameHandlerResults.answers.length} questions and scored ${gameHandlerResults.score} points!`
 
-    const resultsTable = document.querySelector('#tableWithResults');
+    const resultsTable = document.querySelector('#tableWithResults table');
 
-    for (let question of gameHandlerResults.answers) {
+    for (let questionItem of gameHandlerResults.answers) {
         
         const tableCell = document.createElement('tr');
         tableCell.className = 'tableWithResultsQA';
-        tableCell.innerHTML = 
-        `<td><img src='./static/assets/ui/pikach1.png' alt='Pokemon' class='tableWithResultsQuestion'/></td>
-        <td class='tableWithResultsCorrectAnswer'>${question.correctAnswer}</td>
-        <td class='tableWithResultsYourAnswer'>${question.answer}</td>`
+        if ((questionItem.question).substring(0, 3) !== 'http') {
+            tableCell.innerHTML = 
+            `<td>${questionItem.question}</td>
+            <td><img src='${questionItem.correctAnswer}' alt='pokemon img' class='tableWithResultsCorrectAnswer'/></td>
+            <td><img src='${questionItem.answer}' alt='pokemon img' class='tableWithResultsYourAnswer'/></td>`
 
-        if (question.isCorrect === true) {
-            tableCell.querySelector('.tableWithResultsYourAnswer').style.color = 'green';
+            if (questionItem.isCorrect === true) {
+                tableCell.querySelector('.tableWithResultsYourAnswer').style.border = '2px solid green';
+            } else {
+                tableCell.querySelector('.tableWithResultsYourAnswer').style.border = '2px solid red';
+            }
         } else {
-            tableCell.querySelector('.tableWithResultsYourAnswer').style.color = 'red';
+            tableCell.innerHTML = 
+            `<td><img src='${questionItem.question}' alt='pokemon img' class='tableWithResultsQuestion'/></td>
+            <td class='tableWithResultsCorrectAnswer'>${questionItem.correctAnswer}</td>
+            <td class='tableWithResultsYourAnswer'>${questionItem.answer}</td>`
+            
+            if (questionItem.isCorrect === true) {
+                tableCell.querySelector('.tableWithResultsYourAnswer').style.color = 'green';
+            } else {
+                tableCell.querySelector('.tableWithResultsYourAnswer').style.color = 'red';
+            }
         }
-
         resultsTable.appendChild(tableCell);
     }
 }
