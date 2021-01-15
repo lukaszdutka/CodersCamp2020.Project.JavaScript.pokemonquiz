f14489dc8c058f9422c90f8883ce70b0f135894e
AgataLudwiczynska <74932979+AgataLudwiczynska@users.noreply.github.com>
2021-01-15 19:43:40 +0100

    MNG-55 Bugs fixed (#39)
    * Bugs fixed
    
    * Less code
    
    * delete wrong part of code

--- src/app/App.js
+++ src/app/App.js
@@ -10,31 +10,37 @@ export const App = ({options}) => {
    showStartingPage();
 
    //add event listener to the help button 
+   const leader = document.querySelector('#leaderboardScreen');
+   const styleL = getComputedStyle(leader);
    document.querySelector('#helpOption').addEventListener('click', () => {
-      showAPopUpScreen(document.querySelector('#helpScreen'), 'initial');
+      if(styleL.display=='none')
+         showAPopUpScreen(document.querySelector('#helpScreen'), 'initial');
    });
 
    //add event listener to the select mode menu button 
-   const help=document.querySelector('#helpScreen')
-   const style = getComputedStyle(help);
+   const help = document.querySelector('#helpScreen');
+   const styleH = getComputedStyle(help);
    document.querySelector('#whoIsThatPokemonOption').addEventListener('click',()=>{
-      if(style.display=='none'||help.style.display == 'none')
+      if(styleH.display=='none' && styleL.display=='none'){
          console.log("Who's that Pokemon?");
-         SELECTED_MODE = WHO_IS_THAT_POKEMON;
+         SELECTED_MODE = WHO_IS_THAT_POKEMON; 
+      }
    });
    document.querySelector('#whatItLooksLikeOption').addEventListener('click',()=>{
-      if(style.display=='none'||help.style.display == 'none')
+      if(styleH.display=='none' && styleL.display=='none'){
          console.log("What it looks like?");
          SELECTED_MODE = WHAT_DOES_THIS_POKEMON_LOOK_LIKE;
+      }
    });
    document.querySelector('#guessTheTypeOption').addEventListener('click',()=>{
-      if(style.display=='none'||help.style.display == 'none')
+      if(styleH.display=='none' && styleL.display=='none'){
          console.log("Guess the type!");
+      }
    });
 
    //ad event listener to the leaderboard button
    document.querySelector('#leaderboard').addEventListener('click',()=>{
-      if(style.display=='none'||help.style.display == 'none') {
+      if(styleH.display=='none') {
          console.log("Hall of Fame");
          showAPopUpScreen(document.querySelector('#leaderboardScreen'), 'initial')
       }  
