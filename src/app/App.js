670df48516186fbbbda53756e45c33f63b1e543b
Kamil Arendarczyk <kamilarendarczyk1@gmail.com>
2021-01-16 13:01:29 +0100

    add variables

--- src/app/App.js
+++ src/app/App.js
@@ -10,82 +10,65 @@ export const App = ({options}) => {
    showStartingPage();
 
    //add event listener to the help button 
-   const leader = document.querySelector('#leaderboardScreen');
-   const styleL = getComputedStyle(leader);
    document.querySelector('#helpOption').addEventListener('click', () => {
-      if(styleL.display=='none')
-         showAPopUpScreen(document.querySelector('#helpScreen'), 'initial');
+      showAPopUpScreen(document.querySelector('#helpScreen'), 'initial');
    });
 
    //add event listener to the select mode menu button 
-
    const help=document.querySelector('#helpScreen')
    const style = getComputedStyle(help);
    
-   const changeColorOfClickedButton = (querySelector) => {
+   //checked box colors
+   const checkedFont="#3762AC"
+   const bgBoxColor="#FFCB05"
 
-      const whoIsThatPokemonOption = document.querySelector('#whatItLooksLikeOption')
-      const whatItLooksLikeOption = document.querySelector('#guessTheTypeOption')
-      const guessTheTypeOption = document.querySelector('#whoIsThatPokemonOption')
 
-      whoIsThatPokemonOption.style.backgroundColor="#3762AC"
-      whoIsThatPokemonOption.style.color="#FFCB05"
-      whatItLooksLikeOption.style.backgroundColor="#3762AC"
-      whatItLooksLikeOption.style.color="#FFCB05"
-      guessTheTypeOption.style.backgroundColor="#3762AC"
-      guessTheTypeOption.style.color="#FFCB05"
+   const changeColorOfClickedButton = (currQuerySelector) => {
+      const currSelected = document.querySelector(currQuerySelector)
+      const whoIsThatPokemonOption = document.querySelector('#whoIsThatPokemonOption')
+      const whatItLooksLikeOption = document.querySelector('#whatItLooksLikeOption')
+      const guessTheTypeOption = document.querySelector('#guessTheTypeOption')
 
-      querySelector.style.backgroundColor='#FFCB05'
-      querySelector.style.color='#3762AC'
+      whoIsThatPokemonOption.style.backgroundColor=checkedFont
+      whoIsThatPokemonOption.style.color=bgBoxColor
+      whatItLooksLikeOption.style.backgroundColor=checkedFont
+      whatItLooksLikeOption.style.color=bgBoxColor
+      guessTheTypeOption.style.backgroundColor=checkedFont
+      guessTheTypeOption.style.color=bgBoxColor
+
+      currSelected.style.backgroundColor=bgBoxColor
+      currSelected.style.color=checkedFont
     }
     
-
    document.querySelector('#whoIsThatPokemonOption').addEventListener('click',()=>{
-      if(styleH.display=='none' && styleL.display=='none'){
+      if(style.display=='none'||help.style.display == 'none')
          console.log("Who's that Pokemon?");
          SELECTED_MODE = WHO_IS_THAT_POKEMON;
-      }
-       
-         changeColorOfClickedButton(document.querySelector('#whoIsThatPokemonOption'))             
+         changeColorOfClickedButton('#whoIsThatPokemonOption')             
    });
    document.querySelector('#whatItLooksLikeOption').addEventListener('click',()=>{
-      if(styleH.display=='none' && styleL.display=='none'){
+      if(style.display=='none'||help.style.display == 'none')
          console.log("What it looks like?");
-         SELECTED_MODE = WHAT_DOES_THIS_POKEMON_LOOK_LIKE;   
-      }    
-         changeColorOfClickedButton(document.querySelector('#whatItLooksLikeOption'))
+         SELECTED_MODE = WHAT_DOES_THIS_POKEMON_LOOK_LIKE;       
+         changeColorOfClickedButton('#whatItLooksLikeOption')
    });
    document.querySelector('#guessTheTypeOption').addEventListener('click',()=>{
       if(style.display=='none'||help.style.display == 'none')
          console.log("Guess the type!");        
-         changeColorOfClickedButton(document.querySelector('#guessTheTypeOption'))        
+         changeColorOfClickedButton('#guessTheTypeOption')    
    });
 
    //ad event listener to the leaderboard button
    document.querySelector('#leaderboard').addEventListener('click',()=>{
-      if(styleH.display=='none') {
+      if(style.display=='none'||help.style.display == 'none') {
          console.log("Hall of Fame");
          showAPopUpScreen(document.querySelector('#leaderboardScreen'), 'initial')
       }  
    });
 
-   
-   //input (disabled "play" button when input name is empty)
-   const inputName = document.querySelector('#enterYourNameInput');
-   const playButton = document.querySelector("#startGameButton");
-   
-   inputName.addEventListener("keyup", () =>{
-      if(inputName.value.length > 0){
-         playButton.disabled = false;
-      }
-      else{
-         playButton.disabled = true;
-      }
-   })
-
    // start the game
-   playButton.addEventListener("click", () => {
-      const userName = inputName.value;
+   document.querySelector("#startGameButton").addEventListener("click", () => {
+      const userName = document.querySelector('#enterYourNameInput').value;
       renderQuizPage(SELECTED_MODE, userName, options.quizMaxTime)
    });
 }
