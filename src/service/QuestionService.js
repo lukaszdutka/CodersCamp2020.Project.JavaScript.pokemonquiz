0faf49daee5874ad80a2a9af480b588cba63f720
Daria Dziubaltowska <daria305@gmail.com>
2021-01-17 20:56:49 +0100

    MNG-69 Change to map

--- src/service/QuestionService.js
+++ src/service/QuestionService.js
@@ -20,14 +20,15 @@ export class QuestionService {
         const answersObj = await Promise.all(pokePromises);
         
         if (mode === WHO_IS_THAT_POKEMON || mode === WHO_IS_THAT_POKEMON_HARD_MODE) {
+
             return shuffleAnswers({
                 question: answersObj[this.correctAnswerIndex].photoUrl , 
-                answers: [ this.capitalize(answersObj[0].name), this.capitalize(answersObj[1].name), this.capitalize(answersObj[2].name), this.capitalize(answersObj[3].name) ], 
+                answers: answersObj.map((answer) => this.getName(answer)), 
                 correctAnswer: { value: this.capitalize(answersObj[this.correctAnswerIndex].name), index: this.correctAnswerIndex}
             })
         } else if (mode === WHAT_DOES_THIS_POKEMON_LOOK_LIKE ) {
             return shuffleAnswers({
-                question: this.capitalize(answersObj[this.correctAnswerIndex].name) , 
+                question: this.getName(answersObj[this.correctAnswerIndex]) , 
                 answers: [ answersObj[0].photoUrl, answersObj[1].photoUrl, answersObj[2].photoUrl, answersObj[3].photoUrl ], 
                 correctAnswer: { value: answersObj[this.correctAnswerIndex].photoUrl, index: this.correctAnswerIndex }
             })
@@ -45,4 +46,8 @@ export class QuestionService {
     capitalize(str) {
         return str.charAt(0).toUpperCase() + str.slice(1);
       }
+
+    getName(answersElem) {
+        return this.capitalize(answersElem.name)
+    }
 }
